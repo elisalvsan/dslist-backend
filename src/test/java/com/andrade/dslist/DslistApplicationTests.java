006a731a091aeb84d2589ddec22a8808e5803ee8
Elisa Andrade <elisalvsan@gmail.com>
2023-07-13 19:08:55 -0300

    Project created

---
+++ src/test/java/com/andrade/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

98e903cb6b01478b58ebc71b18315a88aabf81ab
Elisa Andrade <elisalvsan@gmail.com>
2023-07-13 23:37:45 -0300

    Domain model

---
+++ src/main/java/com/andrade/dslist/entities/Belonging.java
@@ -0,0 +1,41 @@
+package com.andrade.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging(Game game, GameList list, Integer position){
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/andrade/dslist/entities/GameList.java
@@ -0,0 +1,36 @@
+package com.andrade.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

f03252769a911c1e605e34265a8c614dcf8af85a
Elisa Andrade <elisalvsan@gmail.com>
2023-07-14 00:12:59 -0300

    GameDTO, GameById

--- src/main/java/com/andrade/dslist/controllers/GameController.java
+++ src/main/java/com/andrade/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.andrade.dslist.controllers;
 
+import com.andrade.dslist.dto.GameDTO;
 import com.andrade.dslist.dto.GameMinDTO;
 import com.andrade.dslist.entities.Game;
 import com.andrade.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
